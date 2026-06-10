2baab3393d869181b447863403af8eed2542cf9c
Jesús Odreman <jaosodreman@gmail.com>
2026-06-10 19:02:56 -0300

    Error generado para verificar test unitarios y tests E2E

--- carrito.py
+++ carrito.py
@@ -10,17 +10,20 @@ class Carrito:
     def agregar_producto(self, producto):
         self.__productos.append(producto)
         print(f"✅ {producto.nombre} agregado.")
-
+# Error de lógica - El total siempre está 1$ por debajo del valor correcto
     def calcular_total(self):
-        return sum(p.precio for p in self.__productos)
-
+        #return sum(p.precio for p in self.__productos) # -- Original
+        return sum(p.precio for p in self.__productos) - 1 # -- Con error
+# Error de funcionalidad - El sistema no informa al usuario cuando el carrito está vacío. 
     def mostrar_resumen(self):
         if not self.__productos:
-            print("El carrito está vacío.")
+            #print("El carrito está vacío.") # -- Error. (Si comentas esta linea :) ) 
             return
         print("\n--- Resumen de Compra ---")
+# Error de parámetro / formato - Rompe test que verifican formato con dos decimales.
         for p in self.__productos:
-            print(f"- {p.nombre}: ${p.precio:.2f}")
+            #print(f"- {p.nombre}: ${p.precio:.2f}") # -- Original
+            print(f"- {p.nombre}: ${p.precio:.0f}") # -- Con error
         print(f"TOTAL: ${self.calcular_total():.2f}\n")
 
 def menu():
